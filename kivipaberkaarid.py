a403182ec9d0d46c7e9ec13aad1e32c688a04767
mikkreb <mage636@gmail.com>
2021-04-29 18:46:11 +0300

    Finished the game. It is now in a working state.

--- kivipaberkaarid.py
+++ kivipaberkaarid.py
@@ -1,10 +1,42 @@
-import time
+# Rock paper scissors in Estonian
 import random
 
-print("Vastake väikeste tähtedega!")
+print("Vastake väikeste tähemärkidega!")
 
-userresult = input("Kivi paber või käärid? ")
-cpuresult = ""
-results = ['rock', 'paper', 'scissors']
+draw = False
+victory = None
+opponent = ""
+user = input("Kivi paber või käärid? ")
 
-print(type(results))
+def cpuresult():
+    results = ['kivi','paber','käärid']
+    result = results[random.randint(0, 2)]
+    return result
+
+opponent = cpuresult()
+print("Arvuti vastas: " + opponent)
+
+if opponent == user:
+    draw = True
+elif draw != True:
+    if opponent == 'käärid' and user == 'paber':
+        victory = False
+    elif opponent == 'paber' and user == 'käärid':
+        victory = True
+    elif opponent == 'kivi' and user == 'käärid':
+        victory = False
+    elif opponent == 'käärid' and user == 'kivi':
+        victory == True
+    elif opponent == 'paber' and user == 'kivi':
+        victory = False
+    elif opponent == 'kivi' and user == 'paber':
+        victory = True
+
+
+if victory == True:
+    print("Te võitsite!")
+elif victory == False:
+    print("Te kaotasite :(")
+
+if draw == True:
+    print("Te jäite viiki!")
