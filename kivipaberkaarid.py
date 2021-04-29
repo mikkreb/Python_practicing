e32f813ce1e2d9062889cfba0dd397720024678d
mikkreb <mage636@gmail.com>
2021-04-29 19:46:42 +0300

    Added error reporting, when wrong string is inputted. More humanly readable.

--- kivipaberkaarid.py
+++ kivipaberkaarid.py
@@ -12,6 +12,24 @@ def cpuresult():
     results = ['kivi','paber','käärid']
     result = results[random.randint(0, 2)]
     return result
+def compare(o, u):
+    result = None
+    if o == 'käärid' and u == 'paber':
+        result = False
+    elif o == 'paber' and u == 'käärid':
+        result = True
+    elif o == 'kivi' and u == 'käärid':
+        result = False
+    elif o == 'käärid' and u == 'kivi':
+        result == True
+    elif o == 'paber' and u == 'kivi':
+        result = False
+    elif o == 'kivi' and u == 'paber':
+        result = True
+    else:
+        print("Arvuti ei saanud sobivat sisendit!")
+        print("Palun sisestage kivi, paber või käärid!")
+    return result
 
 opponent = cpuresult()
 print("Arvuti vastas: " + opponent)
@@ -19,19 +37,7 @@ print("Arvuti vastas: " + opponent)
 if opponent == user:
     draw = True
 elif draw != True:
-    if opponent == 'käärid' and user == 'paber':
-        victory = False
-    elif opponent == 'paber' and user == 'käärid':
-        victory = True
-    elif opponent == 'kivi' and user == 'käärid':
-        victory = False
-    elif opponent == 'käärid' and user == 'kivi':
-        victory == True
-    elif opponent == 'paber' and user == 'kivi':
-        victory = False
-    elif opponent == 'kivi' and user == 'paber':
-        victory = True
-
+    victory = compare(opponent, user)
 
 if victory == True:
     print("Te võitsite!")
